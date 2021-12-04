eabf2cf909cc3411f63c92d316ab6046e63e6323
ario baskoro <ariobaskoro@arios-MacBook-Pro-2.local>
2021-12-04 22:44:45 +0700

    ilangin percobaan upload button

--- app.js
+++ app.js
@@ -14,20 +14,20 @@ function detail(num, str) {
   mdlPrice.innerHTML = price.innerHTML;
   console.log(image.innerHTML)
 }
-let idNum = 10;
-function upload() {
-  idNum += 1;
-  let artist = prompt('Artist name: ');
-  let picture = prompt('Your painting URL:');
-  let title = prompt('Your painting tittle:');
-  let price = prompt('Name your price: ');
-  let description = prompt('Short description of your painting: ')
-  let item = document.createElement('div');
-  item.classList.add("item-card", "card", "col-lg-3", "col-md-4", "col-sm-6", "col-xs-6");
-  item.innerHTML = `<div class='item-header'><span id='artistName${idNum}' class='brand-name'>${artist}</span><br></div><span id='itemImg${idNum}'><img class='item-images img-fluid card-img-top' src='${picture}' alt='images'></span><br><span class='item-footer'><span id='itemTitle${idNum}' class='item-name'>${title}</span><br><span id='itemPrice${idNum}'class='price-tag'>$ ${price},-</span></span><button onclick="detail(${idNum}, '${description}')" type='button' class='btn btn-primary item-btn' data-bs-toggle='modal' data-bs-target='#exampleModal'>Detail</button>`
-  let maincontent = document.getElementById('main-section');
-  maincontent.prepend(item);
-}
+// let idNum = 10;
+// function upload() {
+//   idNum += 1;
+//   let artist = prompt('Artist name: ');
+//   let picture = prompt('Your painting URL:');
+//   let title = prompt('Your painting tittle:');
+//   let price = prompt('Name your price: ');
+//   let description = prompt('Short description of your painting: ')
+//   let item = document.createElement('div');
+//   item.classList.add("item-card", "card", "col-lg-3", "col-md-4", "col-sm-6", "col-xs-6");
+//   item.innerHTML = `<div class='item-header'><span id='artistName${idNum}' class='brand-name'>${artist}</span><br></div><span id='itemImg${idNum}'><img class='item-images img-fluid card-img-top' src='${picture}' alt='images'></span><br><span class='item-footer'><span id='itemTitle${idNum}' class='item-name'>${title}</span><br><span id='itemPrice${idNum}'class='price-tag'>$ ${price},-</span></span><button onclick="detail(${idNum}, '${description}')" type='button' class='btn btn-primary item-btn' data-bs-toggle='modal' data-bs-target='#exampleModal'>Detail</button>`
+//   let maincontent = document.getElementById('main-section');
+//   maincontent.prepend(item);
+// }
 
 // https://images.unsplash.com/photo-1638290046884-bfbfdfc3af17?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80
 
